ce78fa25907396f7db91cba7bd92c23b8784658b
davidvujic <david@vujic.se>
2021-01-08 00:01:20 +0100

    fix: handler endpoint api according to RequestStreamHandler

--- src/app/handler.clj
+++ src/app/handler.clj
@@ -1,7 +1,22 @@
 (ns app.handler
-  (:gen-class
-   :methods [^:static [handler [String] String]]))
+  (:require [clojure.java.io :as io]
+            [clojure.data.json :as json]
+            [clojure.walk :as walk]))
 
-(defn -handler [s]
-  (println "Invoking handler. Input argument is" s)
-  (str "Hello world from handler: " s))
+(gen-class
+ :name "app.handler"
+ :implements [com.amazonaws.services.lambda.runtime.RequestStreamHandler])
+
+(defn handler [data]
+ (-> data
+     walk/keywordize-keys
+     :message
+     (or "No message, I'm afraid ...")))
+
+(defn -handleRequest [_this input output _context]
+  (with-open [reader (io/reader input)
+              writer (io/writer output)]
+    (-> reader
+        (json/read)
+        handler
+        (json/write writer))))
